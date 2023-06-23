ca818153f781fde1625aa3f10a65560bd4cff979
VWaghray <87341415+VWaghray@users.noreply.github.com>
2023-06-23 19:16:43 -0400

    Update project-232.py

--- project-232.py
+++ project-232.py
@@ -9,13 +9,13 @@ x=data_set[:,1:7]
 y=data_set[:,8]
 
 model=Sequential()
-model.add(Dense(12, input_dim=8,activation='relu'))
+model.add(Dense(12, input_dim=6,activation='relu'))
 model.add(Dense(8,activation='relu'))
 model.add(Dense(1,activation='sigmoid'))
 model.summary()
 
-predictions=numpy.argmax(model.predict(x), axis=-1)
 model.compile(loss='binary_crossentropy', metrics=['accuracy'])
 model.fit(x,y,epochs=250,batch_size=100)
+predictions=numpy.argmax(model.predict(x), axis=-1)
 for i in range(785,800):
 	print(f'{x[i].tolist()} => {predictions[i]} expected {y[i]}')

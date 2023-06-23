8d9d2d767b84da9090e06fbab563a8086828cb26
VWaghray <87341415+VWaghray@users.noreply.github.com>
2023-06-23 13:31:07 -0400

    Add files via upload

---
+++ project-232.py
@@ -0,0 +1,21 @@
+from numpy import loadtxt
+import numpy
+from keras.models import Sequential
+from keras.layers import Dense
+
+data_set = loadtxt('pokemon.csv', delimiter=',')
+
+x=data_set[:,1:7]
+y=data_set[:,8]
+
+model=Sequential()
+model.add(Dense(12, input_dim=8,activation='relu'))
+model.add(Dense(8,activation='relu'))
+model.add(Dense(1,activation='sigmoid'))
+model.summary()
+
+predictions=numpy.argmax(model.predict(x), axis=-1)
+model.compile(loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(x,y,epochs=250,batch_size=100)
+for i in range(785,800):
+	print(f'{x[i].tolist()} => {predictions[i]} expected {y[i]}')
